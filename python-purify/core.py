44de83a77d82a515061c40187afbe1cea14c51d0
kory donati <koryd@bixly.com>
2015-11-04 16:22:54 -0800

    fixed a few bugs and added import to __init__

--- python-purify/core.py
+++ python-purify/core.py
@@ -1,7 +1,10 @@
 import requests
-from simplejson import loads, dumps
+try:
+    from simplejson import loads, dumps
+except ImportError:
+    from json import loads, dumps
 
-request_string = "http://api1.webpurify.com/services/rest/?method=webpurify.%s.%s&api_key=%s&rsp=1&format=json%s"
+request_string = "http://api1.webpurify.com/services/rest/?method=webpurify.%s.%s&api_key=%s&format=json%s"
 
 class Purify():
     def __init__(self, api_key, live=True):
@@ -23,7 +26,7 @@ class Purify():
         req = requests.get(url)
         return loads(req.content)
 
-    def return(self, text, semail=1, sphone=1, slink=1):
+    def wp_return(self, text, semail=1, sphone=1, slink=1):
         url = request_string % ("live" if self.live else "sandbox", "return", self.api_key, "&semail=%d&sphone=%d&slink=%d&text=%s" % (semail, sphone, slink, text))
         req = requests.get(url)
         return loads(req.content)
