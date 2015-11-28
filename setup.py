d72a6f08e40a374bf34a7c7cddc49ac67c90dd75
kory donati <koryd@bixly.com>
2015-11-27 16:15:59 -0800

    added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
+
+with open('README.rst', 'r') as fid:
+  long = fid.read()
+
 setup(
   name='python_purify',
   packages=['python_purify'],
   version='1.0.1',
   description='A python API for Web Purify',
+  long_description=long,
   author='Tom King, Kory Donati',
   author_email=['tomk@bixly.com', 'koryd@bixly.com'],
   url='https://github.com/kingthomasc/python-purify',
