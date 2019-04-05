1b8ee6935a1c361b9f174ce8c84a627b2e34fac8
kd2718 <korydonati@gmail.com>
2019-04-04 22:21:25 -0700

    fixed error

--- setup.py
+++ setup.py
@@ -6,20 +6,21 @@ here = path.abspath(path.dirname(__file__))
 version_num = "1.2.0"
 
 with open(path.join(here, 'README.rst')) as fid:
-  long_desc = fid.read()
+    long_desc = fid.read()
 
 setup(
-  name='python_purify',
-  packages=['python_purify'],
-  version=version_num,
-  description='A python API for Web Purify',
-  long_description=long_desc,
-  author='Tom King, Kory Donati',
-  author_email=['tomk@bixly.com', 'koryd@bixly.com'],
-  url='https://github.com/kingthomasc/python-purify',
-  download_url='https://github.com/kingthomasc/python-purify/tarball/{ver}}'.format(ver=version_num),
-  keywords=['profanity', 'filter', 'web purify', 'webpurify'],
-  classifiers=[
+    name='python_purify',
+    packages=['python_purify'],
+    version=version_num,
+    description='A python API for Web Purify',
+    long_description=long_desc,
+    author='Tom King, Kory Donati',
+    author_email=['tomk@bixly.com', 'koryd@bixly.com'],
+    url='https://github.com/kingthomasc/python-purify',
+    download_url='https://github.com/kingthomasc/python-purify/tarball/{ver}'.format(
+        ver=version_num),
+    keywords=['profanity', 'filter', 'web purify', 'webpurify'],
+    classifiers=[
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
