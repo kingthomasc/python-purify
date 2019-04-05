9cedfa9390ee30ef50404bd53f05d62bc55e61f7
kd2718 <korydonati@gmail.com>
2019-04-04 22:44:28 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from distutils.core import setup
+#from distutils.core import setup
 from os import path
-
+import setuptools
 here = path.abspath(path.dirname(__file__))
 
 version_num = "1.2.0"
@@ -8,7 +8,7 @@ version_num = "1.2.0"
 with open(path.join(here, 'README.rst')) as fid:
     long_desc = fid.read()
 
-setup(
+setuptools.setup(
     name='python_purify',
     packages=['python_purify'],
     version=version_num,
